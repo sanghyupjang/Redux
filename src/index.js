d4a0020feb29c36e84383f0bfb76e91e453fc51e
Sanghyup <sanghyeob93@naver.com>
2022-03-30 09:56:31 +0900

    push

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-
-ReactDOM.render(<App></App>, document.getElementById("root"));
 ReactDOM.render(
   <Provider store={store}>
     <App />
