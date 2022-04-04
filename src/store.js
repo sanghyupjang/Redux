d8cd93e149cd2f3712398e375defeb669f0b11a6
Sanghyup <sanghyeob93@naver.com>
2022-04-04 11:04:56 +0900

    push

--- src/store.js
+++ src/store.js
@@ -1,22 +1,10 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 
 
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-/* const reducer = (state = [], action) => {
-  switch (action.type) {  
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}; */
 
 const reducer = createReducer([], {
   [addToDo]: (state, action) => {
@@ -26,7 +14,7 @@ const reducer = createReducer([], {
     state.filter(toDo => toDo.id !== action.payload)
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
@@ -34,3 +22,4 @@ export const actionCreators = {
 };
 
 export default store;
+
